9c6f2c6303d45bb2a666922b61a1cd16a38f3428
takker99 <37929109+takker99@users.noreply.github.com>
2024-08-31 18:22:48 +0900

    test: Add the test of an empty .zip

--- fflate/mod.test.ts
+++ fflate/mod.test.ts
@@ -2,6 +2,29 @@ import { expectOk, unzip, zip } from "./mod.ts";
 import { assertEquals } from "@std/assert";
 
 Deno.test("zip and unzip", async (t) => {
+  await t.step("empty", () => {
+    const zipped = expectOk(zip({}));
+    assertEquals(
+      zipped,
+      new Uint8Array(
+        new Uint16Array([
+          0x4b50,
+          0x0605, // end of central directory signature
+          0, // number of this disk
+          0, // number of the disk with the start of the central directory
+          0, // total number of entries in the central dir on this disk
+          0, // total number of entries in the central dir
+          0,
+          0, // size of the central directory
+          0,
+          0, // offset of start of central directory with respect to the starting disk number
+          0, // .ZIP file comment length
+        ]).buffer,
+      ),
+    );
+    assertEquals([...expectOk(unzip(zipped))], []);
+  });
+
   await t.step("text files", () => {
     const obj = {
       "file1.txt": new Uint8Array([72, 69, 76, 76, 79]),
